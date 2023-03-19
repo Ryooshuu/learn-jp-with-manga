8b23900d73b9dc3b813e37959de4545330c9d93b
nao <2980956-Altenhh@users.noreply.gitlab.com>
2023-03-19 13:22:55 -0400

    Set some example code

--- routes/teapot.ts
+++ routes/teapot.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import path from "path";
 import { FileStore } from "../src/database/FileStore";
+import { MangaModelManager } from "../src/database/MangaModelManager";
+import { GetMangaById } from "../src/models/MangaModel";
 import { ApiRequest } from "../src/interfaces/ApiRequest";
 import { ApiResponse } from "../src/interfaces/ApiResponse";
 
@@ -8,8 +10,12 @@ const router = Router();
 
 router.get("/", async (req: ApiRequest, res: ApiResponse) => {
     let store = new FileStore(path.join(__dirname, "..", "files"));
-    await store.Add("test.txt", Buffer.from("meow?"));
-    
+    let modelManager = new MangaModelManager(store);
+
+    let mangaModel = (await GetMangaById("9ddb198f-a621-477c-8b06-86e4fdde782e"))!;
+    await modelManager.SetCover(mangaModel, "test.txt", Buffer.from("baba?"));
+    await modelManager.SetCover(mangaModel, "test.txt", Buffer.from("meow?"));
+
     return res.status(418).json({
         status: 418,
         error: 418,
