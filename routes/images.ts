12b63fd1fa2187d330880f435a3d57e968d17472
nao <2980956-Altenhh@users.noreply.gitlab.com>
2023-03-20 13:06:34 -0400

    View manga cover route

--- routes/images.ts
+++ routes/images.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { AccountModelManager } from "../src/database/AccountModelManager";
+import { MangaModelManager } from "../src/database/MangaModelManager";
 import { ApiRequest } from "../src/interfaces/ApiRequest";
 import { ApiResponse } from "../src/interfaces/ApiResponse";
 import { getAccountById } from "../src/models/Account";
+import { getMangaById } from "../src/models/Manga";
 
 const router = Router();
 
@@ -29,6 +31,33 @@ router.get("/account/:id/avatar", async (req: ApiRequest, res: ApiResponse) => {
 
     let manager = new AccountModelManager();
     let file = manager.getBuffer(account.avatar);
+
+    return res.status(200).send(file);
+});
+
+router.get("/manga/:id/cover", async (req: ApiRequest, res: ApiResponse) => {
+    let manga = await getMangaById(req.params.id);
+
+    if (!manga) {
+        return res.status(404).json({
+            code: 404,
+            message: "The manga you are looking for does not exist."
+        });
+    }
+
+    if (!manga.cover) {
+        // TODO: return a default image.
+        return res.status(404).json({
+            code: 404,
+            message: "The manga you are looking for does not have a cover."
+        })
+    }
+
+    res.setHeader("Content-Type", "image/webp");
+    res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
+
+    let manager = new MangaModelManager();
+    let file = manager.getBuffer(manga.cover);
 
     return res.status(200).send(file);
 });
